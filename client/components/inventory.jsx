0ab71f7c4c5cc8610f8577b05d12a94a943b99b8
Aaron Woo <awoo@chapman.edu>
2021-11-07 20:00:47 +0000

    Updated article compononent to be mobile responsive and centered within the page. #3

--- client/components/inventory.jsx
+++ client/components/inventory.jsx
@@ -14,16 +14,21 @@ export default class Inventory extends React.Component {
 
   render() {
     return (
-    <div className="row d-flex justify-content-center">
-      {
-        this.state.articles.map(article => (
-          <Article articleInfo={article} key={article.articleId}/>
+    <div className="container">
 
-        ))
-      }
-      <Article articleInfo={{ imgUrl: 'images/hoodiePlaceholder.png' }} />
-      <Article articleInfo={{ imgUrl: 'images/hoodiePlaceholder.png' }} />
-      <Article articleInfo={{ imgUrl: 'images/hoodiePlaceholder.png' }} />
+      <div className="row d-flex justify-content-center">
+        {
+          this.state.articles.map(article => (
+            <Article articleInfo={article} key={article.articleId}/>
+
+          ))
+        }
+        <Article articleInfo={{ imgUrl: 'images/hoodiePlaceholder.png' }} />
+        <Article articleInfo={{ imgUrl: 'images/hoodiePlaceholder.png' }} />
+        <Article articleInfo={{ imgUrl: 'images/hoodiePlaceholder.png' }} />
+        <Article articleInfo={{ imgUrl: 'images/hoodiePlaceholder.png' }} />
+
+      </div>
 
     </div>
     );
@@ -33,15 +38,18 @@ export default class Inventory extends React.Component {
 function Article(props) {
   const { imgUrl, primaryColor, secondaryColor } = props.articleInfo;
   return (
-      <div className="col-5 col-md-3 col-lg-2 m-2 mt-4 inventory-article">
-        <img src={imgUrl} className="img-thumbnail border-2 border-dark" />
+      <div className="col-5 col-md-4 col-lg-3 m-1 mt-4 inventory-article m-auto d-flex flex-column align-items-center">
         <div className="row">
           <div className="col-12">
-          <a><div className="primary-square d-inline-block me-2" style={{ backgroundColor: `${primaryColor}` }}></div></a>
-          <a><div className="secondary-square mt-3 d-inline-block" style={{ backgroundColor: `${secondaryColor}` }} ></div></a>
+            <img src={imgUrl} className="img-thumbnail border-2 border-dark" />
           </div>
         </div>
-
+        <div className="row d-flex align-self-start ms-md-3 ms-lg-4 ms-xl-5">
+        <div className="col-12 ps-lg-1 ps-xl-4">
+            <a><div className="primary-square d-inline-block me-2" style={{ backgroundColor: `${primaryColor}` }}></div></a>
+            <a><div className="secondary-square mt-3 d-inline-block" style={{ backgroundColor: `${secondaryColor}` }} ></div></a>
+          </div>
+        </div>
       </div>
   );
 }

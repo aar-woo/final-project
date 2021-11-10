e86e08842a07402651b19aa071536c1abfd04b2a
Aaron Woo <awoo@chapman.edu>
2021-11-10 21:18:41 +0000

    Created outfit picker component. #6

--- client/components/article-options.jsx
+++ client/components/article-options.jsx
@@ -75,89 +75,59 @@ export default class ArticleOptions extends React.Component {
   }
 
   render() {
-    return (
-    // <div className="container mt-5">
-    //   <div className="row d-flex justify-content-center">
-    //     <div className="col-8 col-md-6 d-flex justify-content-center">
-    //         <Carousel
-    //           activeIndex={this.state.activeIndex}
-    //           next={this.next}
-    //           previous={this.previous}
-    //           interval={null}
-    //         >
-    //           <CarouselIndicators
-    //             activeIndex={this.state.activeIndex}
-    //             items={this.state.articleOptions}
-    //             onClickHandler={this.goToIndex}
-    //           />
-    //           {
-    //             this.state.articleOptions.map(item => (
-    //               <CarouselItem key={item.src} className="article-option">
-    //                 <img src={item.src} alt={item.altText} className="border border-2 border-dark"/>
-    //               </CarouselItem>
-    //             ))
-    //           }
-    //           <CarouselControl
-    //             direction="prev"
-    //             directionText="Previous"
-    //             onClickHandler={this.previous}
-    //           />
-    //           <CarouselControl
-    //             direction="next"
-    //             directionText="Next"
-    //             next={this.handleNext}
-    //             onClickHandler={this.next}
-    //           />
-    //         </Carousel>
-    //     </div>
-    //     <div className="card-body col-6 col-md-4 d-flex justify-content-center">
-    //       <ColorSelect classes="col-8 col-md-12" selectClasses='form-select' colorCategory={this.state.colorCategory} value={this.state.colorCategory}
-    //         colorCategorySelect='Color' />
-    //     </div>
-    //   </div>
-    // </div>
+    const numItems = ` ${this.state.articleOptions.length} Items`;
 
-      <div className="container mt-5">
-        <div className="card border-dark">
-          <div className="row ps-md-1 g-0 d-flex justify-content-center justify-content-md-start">
-            <div className="col-12 col-md-4 d-flex justify-content-center">
-                <Carousel
+    return (
+      <div className="container container-max-width mt-5">
+        <div className="card border-dark border-2">
+          <div className="row g-0 d-flex justify-content-center justify-content-md-start">
+            <div className="col-12 col-md-4 d-flex justify-content-center justify-content-md-start">
+              <Carousel
+                activeIndex={this.state.activeIndex}
+                next={this.next}
+                previous={this.previous}
+                interval={null}
+              >
+                <CarouselIndicators
                   activeIndex={this.state.activeIndex}
-                  next={this.next}
-                  previous={this.previous}
-                  interval={null}
-                >
-                  <CarouselIndicators
-                    activeIndex={this.state.activeIndex}
-                    items={this.state.articleOptions}
-                    onClickHandler={this.goToIndex}
-                  />
-                  {
-                    this.state.articleOptions.map(item => (
-                      <CarouselItem key={item.src}>
-                        <img src={item.src} alt={item.altText} className="border border-2 border-dark" />
-                      </CarouselItem>
-                    ))
-                  }
-                  <CarouselControl
-                    direction="prev"
-                    directionText="Previous"
-                    onClickHandler={this.previous}
-                  />
-                  <CarouselControl
-                    direction="next"
-                    directionText="Next"
-                    next={this.handleNext}
-                    onClickHandler={this.next}
-                  />
-                </Carousel>
+                  items={this.state.articleOptions}
+                  onClickHandler={this.goToIndex}
+                />
+                {
+                  this.state.articleOptions.map(item => (
+                    <CarouselItem key={item.src}>
+                      <img src={item.src} alt={item.altText} className="border border-2 border-dark" />
+                    </CarouselItem>
+                  ))
+                }
+                <CarouselControl
+                  direction="prev"
+                  directionText="Previous"
+                  onClickHandler={this.previous}
+                />
+                <CarouselControl
+                  direction="next"
+                  directionText="Next"
+                  next={this.handleNext}
+                  onClickHandler={this.next}
+                />
+              </Carousel>
             </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="d-none d-sm-block"><u>Outfit Picker</u></h5>
-                  <ColorSelect classes="col-12" selectClasses='form-select' colorCategory={this.state.colorCategory} value={this.state.colorCategory}
-                    colorCategorySelect='Color' />
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="d-none d-sm-block"><u>Outfit Picker</u></h5>
+                <ColorSelect classes="col-9 col-md-12 mx-auto mx-md-0" selectClasses='form-select' colorCategory={this.state.colorCategory} value={this.state.colorCategory}
+                  colorCategorySelect='Color' />
+                <div className="col-9 col-md-12 mx-auto d-flex">
+                  <div className="col-6 d-flex align-items-end mt-2">
+                    <a><div className="primary-square" style={{ backgroundColor: `${this.state.primaryColor}` }}></div></a>
+                    <a><div className="secondary-square ms-2" style={{ backgroundColor: `${this.state.secondaryColor}` }}></div></a>
+                  </div>
+                  <div className="col-6 d-flex align-items-end justify-content-end">
+                    <span>{numItems}</span>
+                  </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>

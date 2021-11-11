552ee6ec8204c713cfe7132cd49e020cbf70faf9
Aaron Woo <awoo@chapman.edu>
2021-11-11 21:14:54 +0000

    Updated picker page component to have state properties for each articleId. #8

--- client/pages/picker-page.jsx
+++ client/pages/picker-page.jsx
@@ -3,14 +3,51 @@ import Navbar from '../components/navbar';
 import AppDrawer from '../components/app-drawer';
 import ArticleOptions from '../components/article-options';
 
-export default function PickerPage(props) {
-  return (
-    <>
-      <Navbar pageHeader='Outfit Picker' />
-      <AppDrawer />
-      <ArticleOptions articleType='tops'/>
-      <ArticleOptions articleType='bottoms'/>
-      <ArticleOptions articleType='shoes'/>
-    </>
-  );
+export default class PickerPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topArticleId: null,
+      bottomArticleId: null,
+      shoesArticleId: null
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar pageHeader='Outfit Picker' />
+        <AppDrawer />
+        <div className="container">
+          <ArticleOptions articleType='tops' />
+          <ArticleOptions articleType='bottoms' />
+          <ArticleOptions articleType='shoes' />
+          <div className="row my-3">
+            <div className="col-12 d-flex justify-content-center">
+              <button className="btn btn-primary">Add to Outfits</button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
+
+// export default function PickerPage(props) {
+//   return (
+//     <>
+//       <Navbar pageHeader='Outfit Picker' />
+//       <AppDrawer />
+//       <div className="container">
+//         <ArticleOptions articleType='tops' />
+//         <ArticleOptions articleType='bottoms' />
+//         <ArticleOptions articleType='shoes' />
+//         <div className="row my-3">
+//           <div className="col-12 d-flex justify-content-center">
+//             <button className="btn btn-primary">Add to Outfits</button>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }

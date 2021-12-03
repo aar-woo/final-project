a004750ccf3d941378b3d68545ac41dac2aab28a
Aaron Woo <awoo@chapman.edu>
2021-12-03 17:53:29 +0000

    Refactored server index.js auth sign-up and sign-in to use async await.

--- server/index.js
+++ server/index.js
@@ -22,19 +22,6 @@ app.use(staticMiddleware);
 
 app.use(express.json());
 
-// async function queryDatabase(sql, params, res, next) {
-//   try {
-//     const results = await db.query(sql, params);
-//     if (results.rows.length === 0) {
-//       res.json([]);
-//       return;
-//     }
-//     res.json(results.rows);
-//   } catch (err) {
-//     next(err);
-//   }
-// }
-
 async function queryDatabase(sql, params) {
   const results = await db.query(sql, params);
   return results.rows;
@@ -45,22 +32,21 @@ app.post('/api/auth/sign-up', (req, res, next) => {
   if (!username || !password) {
     throw new ClientError(400, 'username and password are required');
   }
-  argon2
-    .hash(password)
-    .then(hashedPassword => {
-      const sql = `
+  async function signUp() {
+    const hashedPassword = await argon2.hash(password);
+    const sql = `
       insert into "users" ("username", "password")
         values ($1, $2)
         returning "userId", "username"
       `;
-      const params = [username, hashedPassword];
-      return db.query(sql, params);
-    })
-    .then(result => {
-      const [user] = result.rows;
-      res.status(201).json(user);
-    })
-    .catch(err => next(err));
+    const params = [username, hashedPassword];
+    const [newUser] = await queryDatabase(sql, params);
+    res.status(201).json(newUser);
+  }
+  signUp()
+    .catch(err => {
+      next(err);
+    });
 });
 
 app.post('/api/auth/sign-in', (req, res, next) => {
@@ -75,25 +61,24 @@ app.post('/api/auth/sign-in', (req, res, next) => {
      where "username" = $1
   `;
   const params = [username];
-  db.query(sql, params)
-    .then(result => {
-      const [user] = result.rows;
-      if (!user) {
-        throw new ClientError(401, 'invalid login');
-      }
-      const { userId, password } = user;
-      return argon2
-        .verify(password, userPassword)
-        .then(isMatching => {
-          if (!isMatching) {
-            throw new ClientError(401, 'invalid login');
-          }
-          const payload = { userId, username };
-          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
-          res.json({ token, user: payload });
-        });
-    })
-    .catch(err => next(err));
+  async function authenticate() {
+    const [user] = await queryDatabase(sql, params);
+    if (!user) {
+      throw new ClientError(401, 'invalid login');
+    }
+    const { userId, password } = user;
+    const verified = await argon2.verify(password, userPassword);
+    if (!verified) {
+      throw new ClientError(401, 'invalid login');
+    }
+    const payload = { userId, username };
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+    res.json({ token, user: payload });
+  }
+  authenticate()
+    .catch(err => {
+      next(err);
+    });
 });
 
 app.use(authorizationMiddleware);
@@ -114,12 +99,15 @@ app.post('/api/inventory', uploadsMiddleware, (req, res, next) => {
                 returning *
   `;
   const params = [userId, imgUrl, articleTypeId, primaryColor, secondaryColor, colorCategoryId, secondaryColorCategoryId];
-  db.query(sql, params)
-    .then(result => {
-      const [articles] = result.rows;
-      res.status(201).json(articles);
-    })
-    .catch(err => next(err));
+  async function query() {
+    try {
+      const results = await queryDatabase(sql, params);
+      res.status(201).json(results);
+    } catch (err) {
+      next(err);
+    }
+  }
+  query();
 });
 
 app.delete('/api/inventory/:articleId', (req, res, next) => {
@@ -132,15 +120,18 @@ app.delete('/api/inventory/:articleId', (req, res, next) => {
       returning *;
   `;
   const params = [articleId, userId];
-  db.query(sql, params)
-    .then(result => {
-      const [article] = result.rows;
-      if (!article) {
+  async function query() {
+    try {
+      const results = await queryDatabase(sql, params);
+      if (!results) {
         throw new ClientError(404, 'Article not found.');
       }
-      res.status(204).json(article);
-    })
-    .catch(err => next(err));
+      res.status(204).json(results);
+    } catch (err) {
+      next(err);
+    }
+  }
+  query();
 });
 
 app.post('/api/outfits', (req, res, next) => {
@@ -155,7 +146,19 @@ app.post('/api/outfits', (req, res, next) => {
       returning *
   `;
   const params = [topArticleId, bottomArticleId, shoesArticleId, userId];
-  queryDatabase(sql, params, res, next);
+  async function query() {
+    try {
+      const results = await queryDatabase(sql, params);
+      if (results.length === 0) {
+        res.json([]);
+        return;
+      }
+      res.json(results);
+    } catch (err) {
+      next(err);
+    }
+  }
+  query();
 });
 
 app.get('/api/inventory/:userId', (req, res, next) => {
@@ -169,7 +172,6 @@ app.get('/api/inventory/:userId', (req, res, next) => {
       where "userId" = $1
   `;
   const params = [userId];
-  // queryDatabase(sql, params, res, next);
   async function query() {
     try {
       const results = await queryDatabase(sql, params);
@@ -204,7 +206,19 @@ app.get('/api/inventory/:userId/:articleType', (req, res, next) => {
         AND "articleTypeId" = $2
   `;
   const params = [userId, articleTypeId];
-  queryDatabase(sql, params, res, next);
+  async function query() {
+    try {
+      const results = await queryDatabase(sql, params);
+      if (results.length === 0) {
+        res.json([]);
+        return;
+      }
+      res.json(results);
+    } catch (err) {
+      next(err);
+    }
+  }
+  query();
 });
 
 app.get('/api/inventory/:userId/:articleType/:color', (req, res, next) => {
@@ -243,9 +257,10 @@ app.get('/api/inventory/:userId/:articleType/:color', (req, res, next) => {
     AND ("colorCategoryId" = $3 OR "secondaryColorCategoryId" = $3);
   `;
   const params = [userId, articleTypeId, colorId];
-  db.query(sql, params)
-    .then(result => {
-      if (result.rows.length === 0) {
+  async function query() {
+    try {
+      const results = await queryDatabase(sql, params);
+      if (results.length === 0) {
         res.json([{
           imgUrl: `images/${articleType}Placeholder.png`,
           articleId: 0,
@@ -257,9 +272,12 @@ app.get('/api/inventory/:userId/:articleType/:color', (req, res, next) => {
         }]);
         return;
       }
-      res.json(result.rows);
-    })
-    .catch(err => next(err));
+      res.json(results);
+    } catch (err) {
+      next(err);
+    }
+  }
+  query();
 });
 
 app.get('/api/outfits/:userId', (req, res, next) => {
@@ -279,7 +297,19 @@ app.get('/api/outfits/:userId', (req, res, next) => {
     order by "o"."outfitId"
   `;
   const params = [userId];
-  queryDatabase(sql, params, res, next);
+  async function query() {
+    try {
+      const results = await queryDatabase(sql, params);
+      if (results.length === 0) {
+        res.json([]);
+        return;
+      }
+      res.json(results);
+    } catch (err) {
+      next(err);
+    }
+  }
+  query();
 });
 
 app.use(errorMiddleware);

9926a90b06c6f904f26b77b8b5ba87ef0ce53a5b
Aaron Woo <awoo@chapman.edu>
2022-01-21 17:54:00 +0000

    Refactored outfits-page component using hooks.

--- client/pages/outfits-page.jsx
+++ client/pages/outfits-page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Navbar from '../components/navbar';
 import AppDrawer from '../components/app-drawer';
 import Outfit from '../components/outfit';
@@ -6,21 +6,15 @@ import { Spinner } from 'reactstrap';
 import AppContext from '../lib/app-context';
 import Redirect from '../components/redirect';
 
-export default class OutfitsPage extends React.Component {
-  constructor(props) {
-    super(props);
+export default function OutfitsPage(props) {
+  const [outfits, setOutfits] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [networkError, setNetworkError] = useState(false);
+  const { user } = useContext(AppContext);
+  const { token } = useContext(AppContext);
+  const userId = user.userId;
 
-    this.state = {
-      outfits: [],
-      isLoading: true,
-      networkError: false
-    };
-  }
-
-  async componentDidMount() {
-    const userId = this.context.user.userId;
-    const token = this.context.token;
-
+  useEffect(async () => {
     try {
       const response = await fetch(`/api/outfits/${userId}`, {
         headers: {
@@ -37,59 +31,40 @@ export default class OutfitsPage extends React.Component {
           currOutfit = [];
         }
       }
-      this.setState({
-        outfits: outfitsArr,
-        isLoading: false
-      });
+      setOutfits(outfitsArr);
+      setIsLoading(false);
     } catch (err) {
       console.error(err);
-      this.setState({
-        isLoading: false,
-        networkError: true
-      });
+      setIsLoading(false);
+      setNetworkError(true);
     }
+  }, []);
+
+  let page;
+
+  if (isLoading) {
+    page = <Spinner className="mt-5 mx-auto"></Spinner>;
+  } else if (networkError) {
+    page = <h4 className="mt-5 text-center">Sorry, there was an error connecting to the network!</h4>;
+  } else if (outfits.length === 0) {
+    page = <h3 className="text-center mt-5">No outfits saved):</h3>;
+  } else {
+    page = outfits.map((outfit, index) => {
+      return (
+        <Outfit key={outfit[0].outfitId} outfitArticles={outfit} outfitNum={index} />
+      );
+    });
   }
 
-  renderPage() {
-    const outfits = this.state.outfits;
-    if (this.state.isLoading) {
-      return (
-        <Spinner className="mt-5 mx-auto"></Spinner>
-      );
-    }
-    if (this.state.networkError) {
-      return (
-        <h4 className="mt-5 text-center">Sorry, there was an error connecting to the network!</h4>
-      );
-    }
-    if (this.state.outfits.length === 0) {
-      return (
-        <h3 className="text-center mt-5">No outfits saved):</h3>
-      );
-    } else {
-      return (
-        outfits.map((outfit, index) => {
-          return (
-            <Outfit key={outfit[0].outfitId} outfitArticles={outfit}outfitNum={index} />
-          );
-        })
-      );
-    }
-  }
+  if (!user) return <Redirect to="sign-in" />;
 
-  render() {
-    if (!this.context.user) return <Redirect to="sign-in" />;
-
-    return (
-      <>
-        <Navbar pageHeader='Outfits' />
-        <AppDrawer />
-        <div className="row d-flex justify-content-center">
-         { this.renderPage()}
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <Navbar pageHeader='Outfits' />
+      <AppDrawer />
+      <div className="row d-flex justify-content-center">
+        {page}
+      </div>
+    </>
+  );
 }
-
-OutfitsPage.contextType = AppContext;

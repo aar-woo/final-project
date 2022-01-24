d9db13092d7bcf70f9198433ac0e6209827e9518
Aaron Woo <awoo@chapman.edu>
2022-01-24 18:20:28 +0000

    Refactored upload-page component with hooks.

--- client/pages/upload-page.jsx
+++ client/pages/upload-page.jsx
@@ -1,30 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import UploadForm from '../components/upload-form';
 import Navbar from '../components/navbar';
 import Redirect from '../components/redirect';
 import AppContext from '../lib/app-context';
 
-export default class UploadPage extends React.Component {
-  render() {
+export default function UploadPage(props) {
+  const { user } = useContext(AppContext);
 
-    if (!this.context.user) return <Redirect to="sign-in" />;
+  if (!user) return <Redirect to="sign-in" />;
 
-    return (
-      <>
-        <Navbar pageHeader='Upload' />
-        <div className="container mt-5 pt-4">
-          <div className="row d-flex justify-content-center align-items-center">
-            <div className="col-10 col-md-8">
-              <div className="card shadow border border-dark">
-                <UploadForm />
-              </div>
+  return (
+    <>
+      <Navbar pageHeader='Upload' />
+      <div className="container mt-5 pt-4">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-10 col-md-8">
+            <div className="card shadow border border-dark">
+              <UploadForm />
             </div>
           </div>
         </div>
-      </>
-    );
-  }
-
+      </div>
+    </>
+  );
 }
-
-UploadPage.contextType = AppContext;
